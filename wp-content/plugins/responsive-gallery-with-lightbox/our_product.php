113c6b09d0c15d54adfa92835c1896f3b699ab0e
Jon Egana <joneganagranado@gmail.com>
2018-05-04 12:37:22 +0200

    Fixed fatal errors

---
+++ wp-content/plugins/responsive-gallery-with-lightbox/our_product.php
@@ -0,0 +1,141 @@
+<style>
+.ui-tabs, .ui-tabs-panel {
+    background: hsl(0, 0%, 100%) none repeat scroll 0 0;
+    box-shadow: 0 0 6px hsla(0, 0%, 0%, 0.3);
+    margin-left: 100px;
+	margin-top: 50px;
+    min-height: 400px;
+    padding: 15px 32px;
+    width: 81%;
+}
+
+.container h2, .subtitle {
+    font-weight: 400;
+    margin: 0;
+}
+.container h2 {
+    font-size: 23px;
+    font-weight: 400;
+    line-height: 29px;
+    padding: 9px 15px 4px 0;
+}
+.ui-tabs-panel .section {
+    border-bottom: 2px solid hsl(0, 0%, 96%);
+    margin-bottom: 10px;
+    padding-bottom: 40px;
+    padding-top: 20px;
+}
+
+.pricing-three-column {
+    margin: 10px auto 0;
+    width: 100%;
+}
+
+.plan-name {
+    text-align: center;
+}
+.purchase_btn_div {
+    margin-top: 20px;
+    text-align: center;
+}
+.plan-name h2 {
+    font-size: 36px;
+    font-weight: bold;
+    padding-bottom: 10px;
+    padding-top: 30px;
+}
+.plan-name h6 {
+    font-size: 22px;
+    padding-bottom: 10px;
+    padding-top: 10px;
+}
+.container {
+    margin-left: auto;
+    margin-right: auto;
+    padding-left: 15px;
+    padding-right: 15px;
+}
+.purchase_btn_div {
+    margin-top: 20px;
+    text-align: center;
+}
+#option-ourproduct .btn-lg, .btn-group-lg > .btn {
+    border-radius: 2px;
+    font-size: 18px;
+    line-height: 1.33;
+    padding: 10px 16px;
+}
+a.btn-primary, a.btn-inverse {
+    text-decoration: none;
+}
+.dmobtn {
+    margin-right: 10px;
+}
+
+    .pricing-table .plan ul li.plan-feature {  padding: 8px !important;   }
+    .ap .plan-name { background-color: #1E8CBE !important;  }
+    .bp .plan-name { background-color: #1E8CBE !important;  }
+    .cp .plan-name {  background-color: #1E8CBE !important; }
+	li { font-size: larger !important;  }
+    .row-fluid .span4 {       width: 30.624% !important;   }	
+	#product_decs p {font-size:22px;}
+	ol#weblizar_product li a{font-size:18px;}
+</style>
+<div class="block ui-tabs-panel deactive" id="option-ourproduct">
+	<div class="row-fluid pricing-table pricing-three-column">
+		<div class="plan-name centre"> 
+			<a style="margin-bottom:10px;width:200px;textt-align:center" target="_new" href="http://weblizar.com"><img width="200" src="https://weblizar.com/wp-content/uploads/2016/04/logo.png" /></a>
+		</div>	
+		<div class="plan-name">
+			<h2>Weblizar Responsive WordPress Themes & Plugins</h2>
+		</div>
+		<div class="section container">
+			<div class="col-lg-6">
+				<h2>Plugins</h2><hr>
+				<ol id="weblizar_product">
+					<li><a href="https://weblizar.com/plugins/facebook-feed-pro/" target="_new">Facebook Feed Pro</a></li>
+					<li><a href="https://weblizar.com/plugins/pinterest-feed-pro/" target="_new">Pinterest Feed Pro</a></li>
+					<li><a href="https://weblizar.com/plugins/appointment-scheduler-pro/" target="_new">Appointment Scheduler Pro</a></li>
+					<li><a href="https://weblizar.com/plugins/seo-image-optimizer-pro/" target="_new">SEO Image Optimizer Pro</a></li>
+					<li><a href="https://weblizar.com/plugins/newsletter-subscription-form-pro/" target="_new">Newsletter Subscription Form Pro</a></li>
+					<li><a href="https://weblizar.com/plugins/coming-soon-page-maintenance-mode-pro/" target="_new">Coming Soon Page & Maintenance Mode Pro</a></li>
+					<li><a href="https://weblizar.com/plugins/recent-related-post-and-page-pro/" target="_new">Recent Related Post And Page Pro</a></li>
+					<li><a href="https://weblizar.com/plugins/about-author-pro/" target="_new">About Author Pro</a></li>
+					<li><a href="https://weblizar.com/plugins/gallery-pro/" target="_new">Gallery Pro</a></li>
+					<li><a href="https://weblizar.com/plugins/ultimate-responsive-image-slider-pro/" target="_new">Ultimate Responsive Image Slider Pro</a></li>
+					<li><a href="https://weblizar.com/plugins/photo-video-link-gallery-pro/" target="_new">Photo Video Link Gallery Pro</a></li>
+					<li><a href="https://weblizar.com/plugins/flickr-album-gallery-pro/" target="_new">Flickr Album Gallery Pro</a></li>
+					<li><a href="https://weblizar.com/plugins/responsive-portfolio-pro/" target="_new">Responsive Portfolio Pro</a></li>
+					<li><a href="https://weblizar.com/plugins/lightbox-slider-pro/" target="_new">Lightbox Slider Pro</a></li>
+					<li><a href="https://weblizar.com/plugins/responsive-photo-gallery-pro/" target="_new">Responsive Photo Gallery Pro</a></li>				
+				</ol>
+			</div>
+			<div class="col-lg-6">
+				<h2>Themes</h2><hr>
+				<ol id="weblizar_product">
+					<li><a href="https://weblizar.com/themes/realestate-wordpress-premium-theme/" target="_new">RealEstate Premium</a></li>
+					<li><a href="https://weblizar.com/themes/personal-premium-theme/" target="_new">Personal Premium</a></li>
+					<li><a href="https://weblizar.com/themes/newsera-premium-wordpress-theme/" target="_new">Newsera Premium</a></li>
+					<li><a href="https://weblizar.com/themes/fashionair-premium/" target="_new">Fashionair Premium</a></li>
+					<li><a href="https://weblizar.com/themes/corporal-premium/" target="_new">Corporal Premium</a></li>
+					<li><a href="https://weblizar.com/themes/explora-premium/" target="_new">Explora Premium</a></li>
+					<li><a href="https://weblizar.com/themes/scoreline-premium/" target="_new">Scoreline Premium</a></li>
+					<li><a href="https://weblizar.com/themes/beautyspa-premium/" target="_new">Beautyspa Premium</a></li>
+					<li><a href="https://weblizar.com/themes/healthcare/" target="_new">Healthcare Premium</a></li>
+					<li><a href="https://weblizar.com/themes/enigma-premium/" target="_new">Enigma Premium</a></li>
+					<li><a href="https://weblizar.com/themes/creative-premium/" target="_new">Creative Premium</a></li>
+					<li><a href="https://weblizar.com/themes/incredible-premium-theme/" target="_new">Incredible Premium</a></li>
+					<li><a href="https://weblizar.com/themes/chronicle-premium-theme/" target="_new">Chronicle Premium</a></li>
+					<li><a href="https://weblizar.com/themes/guardian-premium-theme/" target="_new">Guardian Premium</a></li>
+					<li><a href="https://weblizar.com/themes/green-lantern-premium-theme/" target="_new">Green Lantern Premium</a></li>
+					<li><a href="https://weblizar.com/themes/weblizar-premium-theme/" target="_new">Weblizar Premium</a></li>
+				</ol>
+			</div>			
+		</div>	
+	</div>											
+	<div class="plan-name centre"> 
+		<div class="purchase_btn_div">
+		  <a href="http://www.weblizar.com" target="_new" class="btn btn-primary btn-lg dmobtn">WWW.WEBLIZAR.COM</a>		
+		</div>
+	</div>
+</div>
